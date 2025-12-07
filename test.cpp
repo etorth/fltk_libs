8cdef301926c2c107269b2d2b17627c4077a8816
etorth <anhonghe@gmail.com>
2025-12-07 10:09:05 -0500

    f

--- test.cpp
+++ test.cpp
@@ -12,16 +12,16 @@ void button_callback(Fl_Widget* widget, void* data) {
 
 int main(int argc, char** argv) {
     Fl_Window* window = new Fl_Window(400, 300, "FLTK Test");
-    
+
     Fl_Box* box = new Fl_Box(50, 50, 300, 100, "FLTK 1.4.4 Static Library Test");
     box->box(FL_UP_BOX);
     box->labelsize(16);
-    
-    Fl_Button* button = new Fl_Button(150, 180, 100, 40, "Click Me");
+
+    Fl_Button* button = new Fl_Button(150, 180, 100, 40, "请点击我！");
     button->callback(button_callback, box);
-    
+
     window->end();
     window->show(argc, argv);
-    
+
     return Fl::run();
 }
